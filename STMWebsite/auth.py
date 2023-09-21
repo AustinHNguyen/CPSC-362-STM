d47b7b36a4f476b2d4669c0ab957731160c5c837
NoahVillanueva <noahvilla22@gmail.com>
2023-09-20 21:23:07 -0700

    Update auth.py file.

--- STMWebsite/auth.py
+++ STMWebsite/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<h>Login</h>"
+    return render_template("login.html") 
 
 @auth.route('/logout')
 def logout():
     return "<h>Logout</h>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
-    return "<h>Sign Up</h>"
+    return render_template("signup.html")
