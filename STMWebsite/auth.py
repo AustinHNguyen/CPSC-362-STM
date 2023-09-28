1ccdafe3d6539fed5dd7c21a24d1387cd04e7a8b
AndrewN95 <ancsuf@csu.fullerton.edu>
2023-09-27 20:58:48 -0700

    Sign up requirements

--- STMWebsite/auth.py
+++ STMWebsite/auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
     return render_template("login.html") 
 
 @auth.route('/logout')
@@ -12,4 +13,23 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password = request.form.get('password')
+        confirmPass = request.form.get('confirmPass')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category = 'error')
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password != confirmPass:
+            flash('passwords do not match.', category = 'error')
+            pass
+        elif len(password < 7):
+            pass
+        else:
+            pass
+
     return render_template("signup.html")
