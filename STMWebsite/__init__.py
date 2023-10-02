331129db517ef00520abd7bdf1fa936d3dc6f9da
AustinHNguyen <123607182+AustinHNguyen@users.noreply.github.com>
2023-10-01 18:03:50 -0700

    Start Database for Users and Tasks.

--- STMWebsite/__init__.py
+++ STMWebsite/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def web_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'CPSC362'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     from .route import route
     from .auth import auth
 
