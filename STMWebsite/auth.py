c9115ea7d0d15f75f1bf9105d5522ca64c8272f4
NoahVillanueva <noahvilla22@gmail.com>
2023-10-02 21:42:17 -0700

    Update auth.py and route.py files.
    Updated login and logout API and also worked on notes page under route.py

--- STMWebsite/auth.py
+++ STMWebsite/auth.py
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
-=======
->>>>>>> 
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-<<<<<<< HEAD
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -19,24 +16,25 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password.', category='error')
         else:
             flash('Email does not exist.', category='error')
 
-=======
->>>>>>> 3dc52a0cd071337ceb92d428c2a732943487ba72
-    return render_template("login.html") 
+    return render_template("login.html", user=current_user) 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<h>Logout</h>"
+    logout_user()
+    return redirect(url_for(auth.login))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
-<<<<<<< HEAD
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
@@ -57,8 +55,7 @@ def signup():
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
-=======
->>>>>>> 3dc52a0cd071337ceb92d428c2a732943487ba72
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
--- STMWebsite/route.py
+++ STMWebsite/route.py
@@ -1,6 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+
 route = Blueprint('views', __name__)
 
 @route.route('/')
+@login_required
 def home_page():
-    return render_template("homepage.html")
+    if  request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("homepage.html", user=current_user)
+
+@route.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = data['noteId']
