3188300eae91951d78c082ee4bcf1df1e6c09ac0
AustinHNguyen <123607182+AustinHNguyen@users.noreply.github.com>
2023-10-04 15:55:19 -0700

    Change Task to Note.

--- STMWebsite/models.py
+++ STMWebsite/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
 
-class Task(db.Model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
