6f51a1c2e7733d8fa724eb14f1574b3feb199ac4
AndrewN95 <ancsuf@csu.fullerton.edu>
2023-10-09 20:59:19 -0700

    Flask Login Module
    half finished

--- STMWebsite/auth.py
+++ STMWebsite/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,16 +16,17 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
             else:
-                flash('Incorrect password.', category='error')
+                flash('Email or password is incorrect.', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Email or password is incorrect.', category='error')
 
-    return render_template("login.html") 
+    return render_template("homepage.html") 
 
 @auth.route('/logout')
 def logout():
-    return "<h>Logout</h>"
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
@@ -35,6 +37,7 @@ def signup():
         password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first()
+        
         if len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
         elif len(first_name) < 2:
