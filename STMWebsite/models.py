a4e6a085fbdb00b900826df0ee8a95ac2f116dbd
AustinHNguyen <123607182+AustinHNguyen@users.noreply.github.com>
2023-10-23 23:40:22 -0700

    Create text boxes.
    Made text boxes with persistent text.

--- STMWebsite/models.py
+++ STMWebsite/models.py
@@ -7,10 +7,11 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+    task = db.Column(db.String(400))
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(1000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    text = db.Column(db.Text)
